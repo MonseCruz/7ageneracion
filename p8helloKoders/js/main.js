4a454a5ad900e8b8d440bc8b738da0fcab5c43c6
MonseCruz <rmonserrat@gmail.com>
2020-03-04 12:18:08 -0600

    2020/03/04 practice 8

---
+++ p8helloKoders/js/main.js
@@ -0,0 +1,15 @@
+var saludo = "hello koders";
+console.log(saludo);
+console.warn("Award Koders");
+console.error("you fail Koder wey");
+console.log(typeof saludo);
+
+
+var x=2;
+var y=10;
+ 
+console.log("la suma de ", x ,"+", y, "es igual a", x + y)
+console.log("la resta de ", x ,"-", y, "es igual a", x - y)
+console.log("la multiplicacion de ", x ,"por", y, "es igual a", x * y)
+console.log("la division de ", x ," entre ", y, "es igual a", x / y)
+
